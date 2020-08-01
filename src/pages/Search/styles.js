37bad9d7eb99475066e0d1ba9d9870c4560b0be9
Felipe Zêba <51774420+felipe-zg@users.noreply.github.com>
2020-08-01 01:06:25 -0300

    filters form animation, showing form when user clicks on filters and hiding whe canceled

--- src/pages/Search/styles.js
+++ src/pages/Search/styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components/native';
+import {Animated} from 'react-native';
 
-export const Form = styled.View`
+export const Form = styled(Animated.View)`
     background: rgba(0, 0, 0, 0.5);
     border-radius: 20px;
     margin: 10px;
@@ -11,4 +12,22 @@ export const Input = styled.TextInput`
     padding: 5px;
     background: #ddd;
     margin-top: 10px;
+`;
+
+export const Button = styled.TouchableOpacity`
+    padding: 10px;
+    background: #5ae4aa;
+    border-radius: 20px;
+    align-items: center;
+    margin-top: 20px;
+`;
+
+export const ShowFormButton = styled.TouchableHighlight`
+    padding: 5px;
+    background: #5ae4aa;
+    border-radius: 20px;
+    align-items: center;
+    margin: 20px 5px 0 0;
+    width: 60px;
+    align-self: flex-end;
 `;
